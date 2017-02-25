a4c03a2b45018354072156afb1a3c522de0e8f4e
Luke Weber <lukedottec@gmail.com>
2017-02-24 19:43:45 -0800

    Added Bitcoin Core repository as training data

---
+++ src/fetch.py
@@ -0,0 +1,10 @@
+# fetch.py
+
+"""
+Code for fetching VCS software repository
+TODO: Implement this, move from hard-coding
+"""
+
+def fetch(repo_name):
+    """ Grab GitHub repository of provided name """
+    pass
---
+++ src/obj.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+# Luke Weber, 11398889
+# Created 12/19/16
+
+"""
+Prior design
+Using Passive-Aggressive classifier to learn abstract skills
+"""
+
+"""
+Proof of Concept design:
+    0. Grab GitHub repository (e.g. Bitcoin Core)
+    1. Parse all files in repo into standard data structure
+    2. Training: Feed 1/2 data to train
+    3. Testing: Feed 1/2 data to test
+    4. Print results
+"""
+
+from sklearn.linear_model import PassiveAggressiveClassifier
+import numpy as np
+import fetch # personal code from fetch.py
+
+def main():
+    
+    # Model and params
+    pac = PassiveAggressiveClassifier()
+    n_samples = 1 # at a time
+    feature_labels = ["Software development", "Web development", "IT", "Social", "Tester"]
+    people_labels = ["Jessica", "Ron", "Patricia", "Qu"]
+
+    # Organization info
+    print("=" * 50)
+    print("ORGANIZATION INFO")
+    print("Population: {0}".format(str(people_labels)))
+    print("Performance metrics: {0}".format(str(feature_labels)))
+    print
+
+    # Train
+    n_runs = 0
+    while True:
+        # Header
+        print("-" * 50)
+
+        # Get task name and features
+        print("TASK BREAKDOWN")
+        task_name = input("Task name> ")
+        print('"' + task_name + '"')
+        task_features = []
+        for f in range(len(feature_labels)):
+            task_features.append(int(input("[{0}] {1}> ".format(f, feature_labels[f]))))
+        X = [task_features] #np.zeros((n_samples, len(feature_labels)))
+
+        # Predict best person for task
+        if n_runs > 0:
+            print
+            print("PREDICT PERSON")
+            pred_p = pac.predict(X)[0]
+            print("Predicted person: {0}".format(people_labels[pred_p]))
+
+        # Get actual best
+        print
+        print("CORRECT PERSON")
+        best_p = int(input("Correct person (0-{0})> ".format(len(people_labels) - 1)))
+        print("Okay, so {0} is the best for this task. Thanks!".format(people_labels[best_p]))
+        y = [best_p]
+
+        # Update model with our 1 sample
+        pac.partial_fit(X, y, range(len(people_labels)))
+
+
+        n_runs += 1
+        print
+        if input("Continue (y/n)?> ") == "n": break
+
+    print("=" * 50)
+
+# Drive!
+main()

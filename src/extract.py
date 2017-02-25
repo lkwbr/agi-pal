f65528dbf4dfaff1c6ce33c8c54ad4dd90d1c12d
Luke Weber <lukedottec@gmail.com>
2017-02-25 00:35:11 -0800

    Last push of the night

--- src/extract.py
+++ src/extract.py
@@ -5,53 +5,68 @@ import queue
 import subprocess
 
 def extract(repo_name):
-	""" Extract sets X and Y from git repository under the folder '/data' """
-	
-	data_dir = "data/"
-	repo_dir = data_dir + repo_name
-	cmd = "git shortlog -s -n"
+    """ Extract sets X and Y from git repository under the folder '/data' """
+   
+    # TODO: Remove the hard-coding
+    data_dir = "../../Repos"
+    os.chdir(data_dir) 
+    repo_dir = repo_name 
 
-	# Go to data location	
-	#os.chdir(repo_dir)
+    # Gather all directory (and subdirectory) files,
+    # and then parse them for features 
+    file_names = walk_dir(repo_dir)
+    X, Y = parse_features(file_names)
 
-	# Walk through whole directory structure
-	file_names = walk_dir(repo_dir)
-	
-	print(len(file_names))
+    return X, Y 
 
-	output = subprocess.getoutput(cmd)
+def parse_features(file_names):
+    """ """
 
-	print(output)
+    X = []
+    Y = []
 
-	return [], []
+    for file_name in file_names:
 
-	# NOTE: May be useful later
-	#os.chdir("C:\Users\DhruvOhri\Documents\COMP 6411\pygithub3-0.3")
-	#os.system("git clone https://github.com/poise/python.git")
+        print(file_name)
+
+        git_cmd = "git log --format=format:%an " + file_name + " | sort | uniq"
+
+        output = subprocess.getoutput(git_cmd)
+        print(output)
+
+    return X, Y
 
 def walk_dir(dir_name):
-	"""
-	Iteratively explore and extract data w/in directory structure. 
-	NOTE: Pervious recursive approach killed stack. 
-	"""
+    """
+    Iteratively explore and extract data w/in directory structure.
+    NOTE: Pervious recursive approach killed stack.
+    """
 
-	#cmd_two = "git log --format=format:%an " + data_dir + " ver1..ver2 | sort | uniq"
+    print(dir_name)
 
-	dir_queue = queue.Queue() 
-	file_bag = []
+    dir_queue = queue.Queue()
+    file_bag = []
 
-	# Walk over current directory
-	dir_queue.put(dir_name)
-	while not dir_queue.empty():
+    # Walk over current directory
+    dir_queue.put(dir_name)
+    while not dir_queue.empty():
 
-		curr_dir = dir_queue.get()
+        curr_dir = dir_queue.get()
 
-		for (dir_path, sub_dir_names, file_names) in os.walk(curr_dir):
-		
-			# Add each subdirectory	to queue to visit later
-			for sub_dir_name in sub_dir_names: dir_queue.put(sub_dir_name)	
+        for (dir_path, sub_dir_names, file_names) in os.walk(curr_dir):
 
-			# Put those filenames in the bag!
-			file_bag.extend(file_names)
+            # Add each subdirectory to queue to visit later
+            for sub_dir_name in sub_dir_names:  
+                dir_queue.put(dir_concat(dir_path, sub_dir_name))
 
-		break
+            # Put those filenames in the bag! Adding directory structure
+            # to name as well
+            file_bag.extend([dir_concat(dir_path, x) for x in file_names])
+
+        print("Scanned directory '" + curr_dir + "'.")
+
+    return file_bag
+
+def dir_concat(dir_pre, dir_post):
+    return dir_pre + "/" + dir_post
+

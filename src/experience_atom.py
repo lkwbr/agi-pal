85c343752aa219241e2f9eaf698bbd245768f8e0
Luke Weber <lukedottec@gmail.com>
2017-03-29 20:10:30 -0700

    Modularizing EA implementation

--- src/experience_atom.py
+++ src/experience_atom.py
@@ -5,11 +5,14 @@
 #   by Audris Mockus et al.
 
 class Delta:
-    """ Represents basic change to source file """
+    """
+    Represents elementary change to source file; can have a single delta per
+    many entities
+    """
 
-    def __init__(self, delta_len, file_name, date, time):
-        self.delta_len = data_len
+    def __init__(self, file_len, delta_len, date, time):
         self.file_name = file_name
+        self.delta_len = data_len
         self.date = date
         self.time = time
 
@@ -20,7 +23,8 @@ class ExperienceAtom:
     """
     (EA) Basic component of experience determined and measured by deltas --
     small changes to any arbitrary file by a particular developer, organization,
-    etc.
+    etc; Each EA is unique to a certain experience gathering entity and to a
+    certain delta
     """
 
     def __init__(self, entity, delta):
